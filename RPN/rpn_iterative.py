b0454c46670457b036a4ce2feff5fd6aefbcc14b
BradlyeG <gordonbradlye@gmail.com>
2019-12-20 09:31:50 -0500

    Continued work on the expression evaluator. Not likely to work and will be significantly changed soon

--- RPN/rpn_iterative.py
+++ RPN/rpn_iterative.py
@@ -1,22 +1,30 @@
+#need math for square root
+import math
+
 #all the operators the user can enter
-valid_operators = ("+", "-", "*", "/","%","^","sqrt","cs","ce")
+valid_operators = ("+", "-", "*", "/","^","sqrt","cs","ce")
 
 #Global variables to keep the current result, and the last entered token to evaluate
-currentResult, stackTop = 0
+currentResult, stackTop, workingRegister = 0
 
+# test method
 def intro():
     print("Welcome to the RPN calculator")
     print("2 ENTER 3 x 1 + = 5")
 
 intro()
 
+#get the expression to evaluate, with each token separated by a space and stored in a list
 def getExpressionInput():
     exp = [str(segment) for segment in input("Please enter an expression:").split()]
     return exp
 
+#sanitize the input to ensure plausible expressions are evaluated
 def sanitizeInput(rawTokens):
 
+    #make a new list and initilize it every time we run this
     sanitizedTokens = []
+    #less than ideal solution to control flow; initilize a binary value to determine when to break out of the token sanitizing loop
     needBreak = False
     
     for token in rawTokens:
@@ -37,8 +45,28 @@ def sanitizeInput(rawTokens):
 def evaluateExpression(operand, expression):
 
     if len(expression) == 1:
-
+        workingRegister = expression[0]
     elif len(expression) > 1:
+        for cntr in len(expression):
+            for op in valid_operators:
+                if expression[cntr] == op:
+                    workingRegister = expression[cntr]
+
+            if workingRegister == "+":
+                currentResult += expression[cntr+1]
+            elif workingRegister == "-":
+                currentResult -= expression[cntr+1]
+            elif workingRegister == "*":
+                currentResult *= expression[cntr+1]
+            elif workingRegister == "/":
+                currentResult /= expression[cntr+1]
+            elif workingRegister == "^":
+                currentResult = currentResult**expression[cntr+1]
+            elif workingRegister == "sqrt":
+                currentResult = math.sqrt(currentResult)
+            elif workingRegister == "cs":
+                
+            elif workingRegister == "ce":
 
 
 
